e2f9e81ad7dcd6c5c3e3cb73d37f86c52f142ee1
BharathAlli <allibharath123@gmail.com>
2018-12-01 21:53:01 -0600

    reactproject

---
+++ my-first-app/users-app-api/src/main/java/com/mycmp/users/payload/VoteRequest.java
@@ -0,0 +1,16 @@
+package com.mycmp.users.payload;
+import javax.validation.constraints.NotNull;
+
+public class VoteRequest {
+    @NotNull
+    private Long choiceId;
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+}
+
